fc1e904c47bf1c35c1172bcc73cd6ed08087eed5
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-03-05 18:08:30 -0500

    created api endpoint for user registration

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', views.test, name='test'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/', include('users.urls', namespace='users')),
 ]
--- core/settings.py
+++ core/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'api',
     'corsheaders',
     'rest_framework',
+    'rest_framework_simplejwt',
     'users'
 ]
 
